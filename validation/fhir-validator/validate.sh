aafc4c4dead0b6e602079cfde54579a5358e36ca
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-10-04 15:25:15 -0700

    update to JSON to XML and remove the terminology txt

--- validation/fhir-validator/validate.sh
+++ validation/fhir-validator/validate.sh
@@ -2,7 +2,6 @@
 java -jar validator_cli.jar "$1" \
   -ig hl7.fhir.us.ecr\#2.1.2 \
   -version 4.0.1 \
-  -output validation_output.xml \
+  -output validation_output.json \
   -html-output validation_output.html \
-  -txLog terminology_log.txt \
   -level warnings
